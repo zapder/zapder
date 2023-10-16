e0bb21f2132799b9ce13d250856acf3ff4083053
Thiti Yamsung <mr.thiti@icloud.com>
2023-10-16 17:42:47 +0700

    refactor: embed workflow

--- src/components/embed/index.ts
+++ src/components/embed/index.ts
@@ -7,4 +7,14 @@ import Youtube from './Youtube';
 
 const allEmbeds = [NewLine, Hashtag, Url, Image, Video, Youtube];
 
+export const replaceEmbed = (content: string) => {
+  let resContent = content;
+
+  allEmbeds.forEach((embed) => {
+    resContent = resContent.replace(embed.regex, embed.component);
+  });
+
+  return resContent;
+};
+
 export default allEmbeds;
